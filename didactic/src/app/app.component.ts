dcfc2ac9a8e3daa3c56428f2ea0a490acdd635ea
Faris <kanddilmoh@yahoo.com>
2020-01-07 21:33:45 -0500

    set up tslint

---
+++ didactic/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lore-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'didactic';
+}
