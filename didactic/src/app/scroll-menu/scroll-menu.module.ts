7117baf901ea63c43b15229cf87781359a3ec403
Faris <kanddilmoh@yahoo.com>
2020-01-08 22:51:59 -0500

    scroll menu created

---
+++ didactic/src/app/scroll-menu/scroll-menu.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ScrollMenuComponent } from './scroll-menu.component';
+
+
+
+@NgModule({
+  declarations: [ScrollMenuComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ScrollMenuModule { }
