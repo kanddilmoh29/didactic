d622f646a7227b6f59cd8cc032abc7137596bffb
Faris <kanddilmoh@yahoo.com>
2020-01-09 23:46:02 -0500

    scroll-menu create didactic components

---
+++ didactic/src/app/didactic/rxjs/rxjs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lore-rxjs',
+  templateUrl: './rxjs.component.html',
+  styleUrls: ['./rxjs.component.scss']
+})
+export class RxjsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
